4d8f3b0168c5ec253305876a0d68b5177a17131f
Tatianna <tatirobinson@gmail.com>
2024-02-07 21:53:18 -0800

    Commit 2667 solution

---
+++ 2667.js
@@ -0,0 +1,23 @@
+
+// 2667. Create Hello World Function
+
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+
+// https://leetcode.com/problems/create-hello-world-function/
+
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
