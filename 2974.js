f5d4bf083b49de5ae741cc6e75baf996833cbc07
Tatianna <tatirobinson@gmail.com>
2024-02-15 19:57:47 -0800

    Commit 2974 solution

---
+++ 2974.js
@@ -0,0 +1,30 @@
+// 2974. Minimum Number Game
+
+// You are given a 0-indexed integer array nums of even length and there is also an empty array arr. Alice and Bob decided to play a game where in every round Alice and Bob will do one move. The rules of the game are as follows:
+
+// Every round, first Alice will remove the minimum element from nums, and then Bob does the same.
+// Now, first Bob will append the removed element in the array arr, and then Alice does the same.
+// The game continues until nums becomes empty.
+// Return the resulting array arr.
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var numberGame = function(nums) {
+    let arr = [];
+    let first;
+    let second;
+    
+    nums = nums.sort((a,b) => a - b)
+    
+    for( let i = 0 ; i < nums.length; i=0){
+        first = nums.shift();
+        second = nums.shift();
+        arr.push(second);
+        arr.push(first);
+    }
+    
+    return arr;
+};
