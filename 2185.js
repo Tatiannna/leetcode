35e4e746bcdbc8bd1d05af7a8043e1e9a8fbbc65
Tatianna <tatirobinson@gmail.com>
2024-02-16 21:42:45 -0800

    Commit 2185 solution

---
+++ 2185.js
@@ -0,0 +1,25 @@
+// 2185. Counting Words With a Given Prefix
+
+
+// You are given an array of strings words and a string pref.
+
+// Return the number of strings in words that contain pref as a prefix.
+
+// A prefix of a string s is any leading contiguous substring of s.
+
+/**
+ * @param {string[]} words
+ * @param {string} pref
+ * @return {number}
+ */
+
+
+var prefixCount = function(words, pref) {
+    let count = 0;
+    
+    for(let i = 0; i < words.length; i++){
+        if(pref === words[i].slice(0, pref.length))count++
+    }
+    
+    return count;
+};
