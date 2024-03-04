a74c12a911458fe17c5e8c9ac823e403050e1454
Tatianna <tatirobinson@gmail.com>
2024-03-03 23:27:44 -0800

    Solve 1832

---
+++ 1832.js
@@ -0,0 +1,21 @@
+// 1832. Check if the Sentence Is Pangram
+
+// A pangram is a sentence where every letter of the English alphabet appears at least once.
+
+// Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+
+// https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+
+
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var checkIfPangram = function(sentence) {
+    let set = new Set()
+    
+    for(let i = 0; i < sentence.length; i++){
+        if(sentence[i] !== ' ') set.add(sentence[i])
+    }
+    return set.size === 26
+};
