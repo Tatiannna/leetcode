da57799e0937603a075a6454ee519fc29162f9a9
Tatianna <tatirobinson@gmail.com>
2024-03-08 23:11:25 -0800

    Comment out instructions - 2540

--- 2540.js
+++ 2540.js
@@ -1,8 +1,8 @@
-2540. Minimum Common Value
+// 2540. Minimum Common Value
 
-Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
+// Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
 
-Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
+// Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
 
 /**
  * @param {number[]} nums1
