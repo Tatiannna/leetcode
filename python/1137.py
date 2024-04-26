a0b4f40af88c151f9f5e0a65bfacb31a4e9944f8
Tatianna <tatirobinson@gmail.com>
2024-04-25 23:57:25 -0700

    Solve 1137

---
+++ python/1137.py
@@ -0,0 +1,19 @@
+# 1137. N-th Tribonacci Number
+
+# The Tribonacci sequence Tn is defined as follows: 
+
+# T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+# Given n, return the value of Tn.
+
+#todo
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        else:
+            return self.tribonacci(n-3) + self.tribonacci(n-1) + self.tribonacci(n-2)
+        
