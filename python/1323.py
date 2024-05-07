f9dc4569dc40e8e50889de26960754d19ca67fb3
Tatianna <tatirobinson@gmail.com>
2024-05-06 23:58:58 -0700

    Solve 1363

---
+++ python/1323.py
@@ -0,0 +1,18 @@
+# 1323. Maximum 69 Number
+
+# You are given a positive integer num consisting only of digits 6 and 9.
+
+# Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        n = list(str(num))
+
+
+        for i in range(0, len(n), -1):
+            if n[i] == '6':
+                n[i] = '9'
+                break
+
+        return int(''.join(n))
