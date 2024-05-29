a866644dc5819e74b1c8d9f08ade593aa4c215d1
Tatianna <tatirobinson@gmail.com>
2024-05-28 23:11:39 -0700

    Solve 234

---
+++ python/234.py
@@ -0,0 +1,23 @@
+
+# 234. Palindrome Linked List
+
+
+# Given the head of a singly linked list, return true if it is a 
+# palindrome or false otherwise.
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
+        p = head
+        s = ''
+
+        while (p):
+            s += str(p.val)
+            p = p.next
+        return s == s[::-1]
