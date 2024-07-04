d06de8ee4c967748c17e4c27a4b443edffd566be
Tatianna <tatirobinson@gmail.com>
2024-07-03 23:31:10 -0700

    Solve 3019

---
+++ python/3019.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countKeyChanges(self, s: str) -> int:
+        count = 0
+
+        for i in range(len(s)-1):
+            if s[i].lower() != s[i+1].lower():
+                count += 1
+
+        return count
+
+        #return len(set(s.lower())) - 1
