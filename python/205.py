fa8328b15c394507de03a9e2f1acd83917a86027
Tatianna <tatirobinson@gmail.com>
2024-07-12 21:31:31 -0700

    Update 205 solution

--- python/205.py
+++ python/205.py
@@ -2,29 +2,32 @@
 # 205. Isomorphic Strings
 
 # Given two strings s and t, determine if they are isomorphic.
-
 # Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-
-# All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
-
-# todo 
+# All occurrences of a character must be replaced with another character while preserving 
+# the order of characters. No two characters may map to the same character, but a character 
+# may map to itself.
 
 class Solution:
+    
     def isIsomorphic(self, s: str, t: str) -> bool:
 
-        sdict = {}
-        tdict = {}
+        ds = {}
+        dt = {}
 
         for i in range(len(s)):
-            if s[i] not in sdict:
-                sdict[s[i]] = 1
-            else:
-                sdict[s[i]] += 1
 
-            if t[i] not in tdict:
-                tdict[t[i]] = 1
-            else:
-                tdict[t[i]] += 1
+                if s[i] not in ds:
+                    ds[s[i]] = [i, 1]
+                else:
+                    ds[s[i]][1] += 1
 
-            if sdict[s[i]] != tdict[t[i]]:
-                return False
+
+                if t[i] not in dt:
+                    dt[t[i]] = [i, 1]
+                else:
+                    dt[t[i]][1] += 1
+                
+                if ds[s[i]] != dt[t[i]]:
+                    return False
+
+        return True  
