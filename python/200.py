70a242303cdc82b243ff17566cca96947aab615a
Tatianna <tatirobinson@gmail.com>
2024-07-27 17:12:48 -0700

    Update 200

--- python/200.py
+++ python/200.py
@@ -10,61 +10,41 @@
 #  edges of the grid are all surrounded by water.
 
 # todo
-
 class Solution:
 
-    # get count of all lands
-    # add to queue, BFS to get all neighbors
-    # if visited is less than the count of all lands, then there is another island
-    # increase island count by 1
-    # if visited count is less than total land count, add an UNVISITED to queue, and BFS
-    # increase island count by 1 
-    # repeat until counts are equal
-
-    def getNeighbors(self, grid, start):
-        row, col = start
-        dirs = [(-1,0), (1,0), (0,1), (0, -1)]
+    def get_neighbors(self, grid, point):
+        x, y = point
+        dirs = [(1,0), (-1, 0), (0,1), (0, -1)]
         neighbors = []
 
-        for d in dirs:
-            dx, dy = d[0] + row, d[1] + col
+        for dx, dy in dirs:
+            nx = x + dx
+            ny = y + dy
 
-            if ( dx >= 0 and dx < len(grid) and dy >= 0 and dy < len(grid[0]) 
-                and grid[dx][dy] == '1'):
-                neighbors.append((dx,dy))
-
+            if ( 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == '1'):
+                neighbors.append((nx, ny))
         return neighbors
-
-        
+            
     def numIslands(self, grid: List[List[str]]) -> int:
 
-        land = []
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == '1':
-                    land.append((i,j))
-
-        count = 1 if land else 0
-
-        queue = [land[0]]
-        #visited = set(land[0])
+        ans = 0
         visited = set()
 
-        while queue:
-            current = queue.pop(0)
-            visited.add(current)
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1' and (i, j) not in visited:
+                    queue = [(i,j)]
 
-            if current in land: # revisit, condition should always be met
-                land.remove(current)
-            neighbors = self.getNeighbors(grid, current)
+                    while queue:
+                        current = queue.pop()
+                        visited.add(current)
 
-            if neighbors:
-                for n in neighbors:
-                    queue.append(n)
-                    visited.add(n)
-            
-            if len(land) > 0:
-                count += 1
-                queue.append(land[0])
+                        neighbors = self.get_neighbors(grid, current)
 
-        return count
+                        for neighbor in neighbors:
+                            if neighbor not in visited:
+                                queue.append(neighbor)
+                                visited.add(neighbor)
+                    ans += 1
+        
+        return ans
