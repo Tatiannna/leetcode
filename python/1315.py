e19a589d221c6d5308e889c7c4aaf157e7c86d17
Tatianna <tatirobinson@gmail.com>
2024-08-12 18:09:32 -0700

    Update 1315 solution

--- python/1315.py
+++ python/1315.py
@@ -2,8 +2,6 @@
 
 # https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/description/
 
-# todo
-
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -13,25 +11,56 @@
 #         self.right = right
 class Solution:
     def sumEvenGrandparent(self, root: TreeNode) -> int:
-        res = 0
-        nodes = defaultdict(list)
+        # res = 0
+        # nodes = defaultdict(list)
 
-        stack = [(root, 1)]
+        # stack = [(root, 1)]
+
+        # while stack:
+        #     current, lvl = stack.pop()
+
+        #     if current.val % 2 == 0:
+        #         nodes[lvl].append(current.val)
+
+        #     if lvl - 2 in nodes:
+        #         print("adding: ", current.val)
+        #         res += current.val
+
+        #     if current.right:
+        #         stack.append((current.right, lvl + 1))
+        #     if current.left:
+        #         stack.append((current.left, lvl + 1))
+        #     if not current.left and not current.right:
+        #         nodes[lvl - 2] = []
+        # return res
+
+         def getGrandSum(node):
+            res = 0
+            if node.left or node.right:
+                if node.left:
+                    if node.left.left:
+                        res += node.left.left.val
+                    if node.left.right:
+                        res += node.left.right.val
+                if node.right:
+                    if node.right.left:
+                        res += node.right.left.val
+                    if node.right.right:
+                        res += node.right.right.val
+            return res
+
+        ans = 0
+
+        stack = [root]
 
         while stack:
-            current, lvl = stack.pop()
-
+            current = stack.pop()
             if current.val % 2 == 0:
-                nodes[lvl].append(current.val)
+                ans += getGrandSum(current)
 
-            if lvl - 2 in nodes:
-                print("adding: ", current.val)
-                res += current.val
-
-            if current.right:
-                stack.append((current.right, lvl + 1))
             if current.left:
-                stack.append((current.left, lvl + 1))
-            if not current.left and not current.right:
-                nodes[lvl - 2] = []
-        return res
+                stack.append(current.left)
+            if current.right:
+                stack.append(current.right)
+
+        return ans
